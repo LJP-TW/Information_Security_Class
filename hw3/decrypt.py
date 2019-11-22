3d1129f96b28fca1add8271368db69d1f4e2acd5
AxotZero <asdswegreh@google.com>
2019-11-22 17:32:18 +0800

    some error in CBC

---
+++ hw3/decrypt.py
@@ -0,0 +1,48 @@
+import numpy as np
+from PIL import Image
+from Crypto.Cipher import AES
+import sys
+
+# get parameters
+mode = sys.argv[1]
+key = sys.argv[2]
+path = sys.argv[3]
+
+# save to temp bmp image
+p = Image.open(path)
+p.save('dec.bmp')
+
+ciphertext = ''
+
+with open("dec.bmp", "rb") as f:
+  f = f.read()
+  #ciphertext = cipher.encrypt(clear)
+  trimmed = f[64:-2]
+
+  paddingNum = 16 - (len(trimmed)%16)
+  if paddingNum > 0:
+  	trimmed = trimmed + '0' * paddingNum
+  
+  if mode == 'ECB':
+  	cipher = AES.new(key, AES.MODE_ECB)
+  	ciphertext = ''.join([cipher.decrypt(trimmed[index: index+16]) for index in range(0, len(trimmed), 16)])
+  else:
+  	IV = '0000111122223333'
+  	for index in range(16, len(trimmed), 16):
+  		cipher = AES.new(key, AES.MODE_CBC, trimmed[index-16: index])
+  		ciphertext = ciphertext + cipher.decrypt(trimmed[index: index+16])
+  		# ciphertext = ciphertext + IV
+  	
+  	cipher = AES.new(key, AES.MODE_CBC, IV)
+  	ciphertext =  cipher.decrypt(trimmed[0:16]) + ciphertext
+
+  ciphertext = ciphertext[:len(ciphertext)-paddingNum]
+  ciphertext = f[0:64] + ciphertext + f[-2:]
+
+
+
+with open("dec.bmp", "w") as f:
+  f.write(ciphertext)
+
+
+print('finish')
